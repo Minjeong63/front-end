4050441cebe7bf6b170e501b4e52b1fe60f61acc
Minjeong63 <kmj06030@naver.com>
2023-02-20 17:51:27 +0900

    2023.02.20

--- App.tsx
+++ App.tsx
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { theme } from "./colors";
+import { useState } from "react";
 
 export default function App() {
+  const [working, setWorking] = useState<boolean>(true);
+  const [text, setText] = useState<string>("");
+  const [toDos, setToDos] = useState<Object>({});
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload: string) => setText(payload);
+  const addToDo = () => {
+    if (!text) return;
+    else {
+      const newToDos = Object.assign({}, toDos, {
+        [Date.now()]: { text, work: working },
+      });
+      setToDos(newToDos);
+      setText("");
+    }
+  };
+  console.log(toDos);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        onChangeText={onChangeText}
+        onSubmitEditing={addToDo}
+        returnKeyType="done"
+        value={text}
+        placeholder={working ? "할 일을 추가해 보세요." : "어디로 가고 싶나요?"}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -13,8 +69,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "700",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
--- README.md
+++ README.md
@@ -5,3 +5,34 @@
 터미널을 켜서 폴더를 생성하고 싶은 위치로 이동한 후
 expo init reactnative_memolist 를 입력하면
 해당 위치에 reractnative_memolist 폴더가 생성됨 (blank - typescript로 생성)
+
+### Button Component (Touchable)
+
+##### TouchableOpacity
+
+누르는 이벤트를 listen할 준비가 된 view라고 할 수 있음, opacity가 있는 이유는 눌렀을 때 애니메이션 효과가 있기 때문
+
+##### TouchableHighlight
+
+- TouchableOpacity보다 더 많은 속성을 가짐, 내가 요소를 클릭했을 때 배경색이 바뀌도록 해줌
+- onPress는 유저가 Touchable을 눌렀을 때 실행되는 이벤트를 말함
+- underlayColor를 지정해야 버튼을 눌렀을 때 색깔이 보임
+
+##### TouchableWithoutFeedback
+
+- 화면의 가장 위에서 일어나는 탭 이벤트를 listen 하는 것
+- 하지만 그래픽이나 다른 UI 반응은 보여주지 않음
+
+##### Pressable
+
+- TouchableWithoutFeedback이랑 비슷함
+- 추가적인 옵션을 더 가지고 있어서 좀 더 섬세한 설정이 가능
+
+### TextInput
+
+- keyboardType : 키보드 타입 지정
+- onChangeText : text가 바꼈는지 확인하는 함수
+- returnKeyType : 키보드에서 입력하는 부분의 모양과 유형을 바꿀 수 있음
+- secureTextEntry : 비밀번호에 input에 사용
+- multiline : 메모장 같이 여러줄을 입력할 때 사용
+- placeholderTextColor : placeholder text 색깔 바꿀 수 있음
